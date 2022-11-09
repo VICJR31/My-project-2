3668384ab94e15b0625c1b3230e7aa8725ede734
VICJR31 <alejandroromo31st@gmail.com>
2022-11-08 17:53:48 -0800

    Test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -41,19 +41,4 @@ public class PlayerMovement : MonoBehaviour
         //rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
     }
     
-//  [SerializeField] private float speed;
-//     private Rigidbody2D body;
- 
-//     private void Awake()
-//     {
-//         body = GetComponent<Rigidbody2D>();
-//     }
- 
-//     private void Update()
-//     {
-//         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
- 
-//         if (Input.GetKey(KeyCode.Space))
-//             body.velocity = new Vector2(body.velocity.x, speed);
-//     }
 }
