ad44d37c21849468963f5bc8de25de325dce762e
Navatronn <113819920+Navatronn@users.noreply.github.com>
2022-12-03 20:29:03 -0800

    Added Game Over Screen and Credits

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static event Action OnPlayerDeath;
     public int maxHealth = 100; 
     public int currentHealth;
     public HealthBar healthBar; 
@@ -20,6 +22,7 @@ public class Player : MonoBehaviour
         healthBar.setHealth(currentHealth);
         if(currentHealth <= 0){
             Die(); 
+            OnPlayerDeath?.Invoke();
         }
     }
     void Die(){
